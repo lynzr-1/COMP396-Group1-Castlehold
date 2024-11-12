6c3ec04abe7eacb7996ca03889a754e82e3ccda0
lynzr-1 <lriches1@my.centennialcollege.ca>
2024-11-12 00:32:30 -0500

    Adjust tower drag & drop; Begin implementing castle health system; Add UI elements

--- Assets/Scripts/EnemyAIScripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyAIScripts/EnemyBehaviour.cs
@@ -5,32 +5,73 @@ using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    [Header("Enemy Settings")]
+    public float damage = 10f;
+    public float speed = 5f;
+
+    public PoolManager poolManager;  // Reference to the pool manager
+
+    #region Private Variables
     protected NavMeshAgent agent;
     protected Animator animator;
+    protected CastleHealthManager castleHealthManager;
     private bool isAttacking;
-    public PoolManager poolManager;  // Reference to the pool manager
     private Renderer enemyRenderer;
+    private Transform endPoint;
+    private float reachThreshold = 1.0f;
+    private bool hasReachedCastle = false; //to ensure reach castle is only called once when the enemy reaches it
+    #endregion
+
+    private void Start()
+    {
+        // Set the end point for the enemy to reach
+        GameObject endPointObject = GameObject.FindGameObjectWithTag("EnemyPathEnd");
+
+        if (endPointObject != null)
+        {
+            endPoint = endPointObject.transform;
+            agent.SetDestination(endPoint.position);  // Use inherited agent from EnemyBehaviour
+        }
+        else
+        {
+            Debug.LogError("End point not found. Please tag the end point with 'EnemyPathEnd'.");
+        }
+    }
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
         enemyRenderer = GetComponentInChildren<Renderer>();
+        castleHealthManager = FindObjectOfType<CastleHealthManager>();
     }
+
+    private void Update()
+    {
+        if (!hasReachedCastle && endPoint != null && Vector3.Distance(transform.position, endPoint.position) <= reachThreshold)
+        {
+            hasReachedCastle = true;
+            Debug.Log("Reached Castle");
+            ReachCastle();  // Call ReachCastle from EnemyBehaviour when reaching the end point
+        }
+    }
+
     public void ReachCastle()
     {
-        Debug.Log("Enemy reached end point");
 
         // Stop the navmesh agent to halt movement at the gate
         agent.isStopped = true;
 
         // Play attack animation
         animator.SetTrigger("Attack");
+        Debug.Log("Calling Take Damage on castle");
+        castleHealthManager.TakeDamage(damage); //call the take damage function on the castle
 
         // Start the fade out coroutine after a delay to allow the attack animation time to play
         StartCoroutine(FadeOutAfterDelay(1.0f));
     }
 
+    #region Attack Anims & Coroutines to Fade After Delay
     public void StartAttack(float duration)
     {
         if (isAttacking) return;  // Prevent multiple attacks at the same time
@@ -75,4 +116,5 @@ public class EnemyBehaviour : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    #endregion
 }
